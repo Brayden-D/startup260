03d1bdf8c1732ad8a5aa67b59dd57f956f202817
Brayden D <braydentdickerson@gmail.com>
2026-02-12 23:04:04 -0700

    leaderboard yay

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
 
 export function Leaderboard() {
   return (
     <main>
-      <div>leaderboard displayed here</div>
+      <br />
+      <br />
+
+      <div className="centerdiv">
+        <div
+          className="centerdiv panel"
+          style={{ width: "50%" }}
+        >
+          <div>
+            <h1>Leaderboard</h1>
+          </div>
+
+          <div style={{ textAlign: "left", width: "85%", height: "50vh" }}>
+            1. User: X in a row <br />
+            2. User: Y in a row
+          </div>
+
+          <br />
+        </div>
+      </div>
     </main>
   );
 }
